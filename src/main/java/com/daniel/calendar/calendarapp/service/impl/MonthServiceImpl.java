ab27fdba13d3032a720fefcaced4f3cec3050846
Bruno Mota <bruno.mota.29@gmail.com>
2019-12-15 15:46:52 +0000

    created Month Service Impl

---
+++ src/main/java/com/daniel/calendar/calendarapp/service/impl/MonthServiceImpl.java
@@ -0,0 +1,17 @@
+package com.daniel.calendar.calendarapp.service.impl;
+
+import com.daniel.calendar.calendarapp.model.MonthDTO;
+import com.daniel.calendar.calendarapp.service.MonthService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MonthServiceImpl implements MonthService {
+
+    @Override
+    public MonthDTO getMonthById(Long monthId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
