82d91386ae09ee3d2dcc5a96bf52f8f0155aac2c
Bruno Mota <bruno.mota.29@gmail.com>
2019-12-15 15:29:38 +0000

    added MonthService interface

---
+++ src/main/java/com/daniel/calendar/calendarapp/service/MonthService.java
@@ -0,0 +1,12 @@
+package com.daniel.calendar.calendarapp.service;
+
+import com.daniel.calendar.calendarapp.model.MonthDTO;
+
+public interface MonthService {
+
+    /**
+     * This is just an example method, could be other
+     */
+    MonthDTO getMonthById(Long monthId);
+
+}
